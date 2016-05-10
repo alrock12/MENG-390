cd1b461b7561d19caccc7ded5d0c63f8ae15d9c6
Amy Rockwood <amy.rockwood@yale.edu>
2016-05-10 14:13:53 -0400

    misc update

--- Lab 4/Pendulum/Pendulum.ino
+++ Lab 4/Pendulum/Pendulum.ino
@@ -6,17 +6,14 @@
 // No need to specify pins for I2C
 Adafruit_L3GD20 gyro;
 
-
-int z_pin = 10;
-int z_dps = 0;
-long int z_pwm = 0;
+int y_dps = 0;
 
 void setup() 
 {
   Serial.begin(9600);
   
   // Try to initialise and warn if we couldn't detect the chip
-  if (!gyro.begin(gyro.L3DS20_RANGE_2000DPS))          //range can be 250,500, or 2000 degrees per second
+  if (!gyro.begin(gyro.L3DS20_RANGE_250DPS))          //range can be 250,500, or 2000 degrees per second
   {
     Serial.println("Oops ... unable to initialize the L3GD20. Check your wiring!");
     while (1);
@@ -29,10 +26,7 @@ void loop()
   //records a data point of [x,y,z] in degrees-per-second
   //Serial.print("Z: "); Serial.println(gyro.data.z); Serial.print(" ");
   delay(10);
-  z_dps = gyro.data.z;//only read magnitude;
-  z_pwm = z_dps/4 + 128; 
-  //Serial.print("z_pwm:  "); 
-  Serial.println(z_pwm);Serial.print(" ");
-  analogWrite(z_pin, z_pwm);
+  y_dps = gyro.data.y;//only read magnitude;
+  Serial.println(y_dps);Serial.print(" ");
   
 }
